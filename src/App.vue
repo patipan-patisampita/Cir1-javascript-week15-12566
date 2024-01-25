add5f7c45a504c31cc166bf9d230090665bbfd51
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-25 16:24:55 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background: #6D08CC;">
+  <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background: #7f00ff;">
     <div class="container">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
