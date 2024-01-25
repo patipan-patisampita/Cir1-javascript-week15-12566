d661a691b5e4da685dd039d8bb2e51fb293d34c4
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-25 16:02:26 +0700

    update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container my-5">
+        <div class="card shadow p-0" style="width: 18rem;">
+            <img src="https://positioningmag.com/wp-content/uploads/2018/01/4_esport.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Home Page</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <a href="#" class="btn btn-primary">Read...</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
