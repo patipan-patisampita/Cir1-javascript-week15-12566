dbc50a2346b8b52111d09c2b4b6573a13a9fdc3e
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-01 15:11:33 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,37 @@
 <template>
     <div class="container my-5">
-        <div class="card shadow p-0" style="width: 18rem;">
-            <img src="https://positioningmag.com/wp-content/uploads/2018/01/4_esport.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">สาขาเทคโนโลยีสารสนเทศ</h5>
-                <p class="card-text">
-                    เพื่อให้สามารถปฏิบัติงานในอาชีพนักพัฒนาซอฟต์แวร์คอมพิวเตอร์หรือ นักพัฒนาระบบเครือข่าย
-                    คอมพิวเตอร์และความปลอดภัย หรือ นักพัฒนาระบบสมองกลฝังตัวและไอโอที หรือ นักพัฒนา
-                    คอมพิวเตอร์กราฟิกเกมส์และแอนิเมชั่น ในสถานประกอบการและประกอบอาชีพอิสระ รวมทั้งการ
-                    ใช้ความรู้และทักษะเป็นพื้นฐานในการศึกษาต่อในระดับสูงขึ้นได้
-                </p>
-                <a href="http://www.trattc.ac.th:4001/" target="_blank" class="btn btn-primary">อ่านต่อ...</a>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card shadow p-0" style="width: 18rem;">
+                    <img src="https://positioningmag.com/wp-content/uploads/2018/01/4_esport.png" class="card-img-top"
+                        alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">สาขาเทคโนโลยีสารสนเทศ</h5>
+                        <p class="card-text">
+                            เพื่อให้สามารถปฏิบัติงานในอาชีพนักพัฒนาซอฟต์แวร์คอมพิวเตอร์หรือ นักพัฒนาระบบเครือข่าย
+                            คอมพิวเตอร์และความปลอดภัย หรือ นักพัฒนาระบบสมองกลฝังตัวและไอโอที หรือ นักพัฒนา
+                            คอมพิวเตอร์กราฟิกเกมส์และแอนิเมชั่น ในสถานประกอบการและประกอบอาชีพอิสระ รวมทั้งการ
+                            ใช้ความรู้และทักษะเป็นพื้นฐานในการศึกษาต่อในระดับสูงขึ้นได้
+                        </p>
+                        <a href="http://www.trattc.ac.th:4001/" target="_blank" class="btn btn-primary">อ่านต่อ...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card shadow">
+                    <div class="card-header">
+                        Header
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">
+                            Title
+                        </div>
+                        <div class="card-text">
+                            Sub Title
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
